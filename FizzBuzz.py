c0acd27eaf3356dd4586b581b17072901229bb38
James <simest45@gmail.com>
2018-11-02 16:48:15 -0400

    11/2 - Added the basic fizzbuzz challenge. Found that it might be possible to one line it with lots of Python play....

---
+++ FizzBuzz.py
@@ -0,0 +1,14 @@
+#Fizz buzzed eg;
+#Write a program that prints the numbers from 1 to 100.
+#But for multiples of three print “Fizz” instead of the number
+#and for the multiples of five print “Buzz”. For numbers which are
+#multiples of both three and five print “FizzBuzz”.
+
+for num in range(1,101):
+    #print(num)
+    if (num % 5) == 0 and (num % 3) == 0:
+        print(f"{num} is FizzBuzz")
+    elif (num % 5) == 0:
+        print(f"{num} is Buzz")
+    elif (num % 3) == 0:
+        print(f"{num} is Fizz")
